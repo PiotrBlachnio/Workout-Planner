efb5e58caf0f684eb014e89023db99c030edf759
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-08-17 15:12:35 +0200

    Create category model

---
+++ Database/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace NotesAPI.Database.Models {
+    public class Category {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CreatedAt { get; set; }
+    }
+}
