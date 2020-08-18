0d5bffe888daa0b7b41e167e318631cb70b8ebe3
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-08-18 09:29:27 +0200

    Create categories profile for automapper

--- Contracts/Responses/CategoryResponse.cs
+++ Contracts/Responses/CategoryResponse.cs
@@ -1,7 +1,5 @@
 namespace NotesAPI.Contracts.Responses {
     public class CategoryResponse {
-        public int Id { get; set; }
-
         public string Name { get; set; }
 
         public long CreatedAt { get; set; }
---
+++ Profiles/CategoriesProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using NotesAPI.Contracts.Requests;
+using NotesAPI.Contracts.Responses;
+using NotesAPI.Database.Models;
+
+namespace NotesAPI.Profiles {
+    public class CategoriesProfile : Profile {
+        public CategoriesProfile() {
+            CreateMap<Category, CategoryResponse>();
+            CreateMap<CreateCategoryRequest, Category>();
+        }
+    }
+}
