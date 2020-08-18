fa903dc29feb46ea332cc7d75956352a50982f65
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-08-18 07:54:24 +0200

    Create note model

---
+++ Database/Models/Note.cs
@@ -0,0 +1,13 @@
+namespace NotesAPI.Database.Models {
+    public class Note {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+        public List<string> Tags { get; set; }
+        public int CreatedAt { get; set; }
+        public int LastEditedAt { get; set; }
+        public int OwnerId { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
